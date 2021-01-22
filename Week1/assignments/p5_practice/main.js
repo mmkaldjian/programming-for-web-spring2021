eda9f83d1960d5312e275b85e28eea767d8e1ced
mmkaldjian <74389945+mmkaldjian@users.noreply.github.com>
2021-01-21 19:11:16 -0600

    more functions

--- Week1/assignments/p5_practice/main.js
+++ Week1/assignments/p5_practice/main.js
@@ -1,15 +1,18 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
   }
-  
+
   function draw() {
     background(280, 55, 87, 5);
 
     if (mouseIsPressed) {
     fill(255, 153, 0);
+    stroke(0, 255, 255);
   } else {
     fill(0, 255, 255);
+    stroke(255, 153, 0);
   }
-    ellipse(mouseX, mouseY, 20, 20);
-
+    arc(mouseX, mouseY, 40, 40, 0, PI + QUARTER_PI, CHORD);
+    strokeJoin(ROUND);
+    strokeWeight(8);
   }
