48b3f31248f2b2773d2703b51233acdf1f0a845c
mmkaldjian <74389945+mmkaldjian@users.noreply.github.com>
2021-01-27 20:55:47 -0600

    movie arrays intial commit

---
+++ Week2/Arrays/movie_array.js
@@ -0,0 +1,7 @@
+const favoriteMovie = window.prompt("what is your favorite movie?");
+
+const movieArray = ["Blindspotting", "Beginners" "Pride and Prejudice", "Sorry to Bother You", "Lady Bird"];
+
+movieArray.push(favoriteMovie);
+console.log(movieArray);
+console.log(movieArray.toString());
