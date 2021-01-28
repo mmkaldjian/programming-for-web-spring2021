57eb54d5dd877f566b205304051b533467b64e7e
mmkaldjian <74389945+mmkaldjian@users.noreply.github.com>
2021-01-27 20:59:48 -0600

    updated movie array js

--- Week2/Arrays/movie_array.js
+++ Week2/Arrays/movie_array.js
@@ -1,6 +1,6 @@
 const favoriteMovie = window.prompt("what is your favorite movie?");
 
-const movieArray = ["Blindspotting", "Beginners" "Pride and Prejudice", "Sorry to Bother You", "Lady Bird"];
+const movieArray = ["Blindspotting", "Beginners", "Pride and Prejudice", "Sorry to Bother You", "Lady Bird"];
 
 movieArray.push(favoriteMovie);
 console.log(movieArray);
