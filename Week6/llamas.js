28a4eb67b1e4ca528fd4cbd7270272582f4cbd50
mmkaldjian <74389945+mmkaldjian@users.noreply.github.com>
2021-02-27 21:07:16 -0600

    llamas initial commit

---
+++ Week6/llamas.js
@@ -0,0 +1,73 @@
+// Add your code here
+// new instance of the Vue class
+
+new Vue({
+    el: "#weight",
+    data: {
+      llamaFact: "Llamas weigh between 280 and 450 pounds."
+    }
+  });
+
+  new Vue({
+    el: "#history",
+    data: {
+      llamaFact: "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands."
+    }
+  });
+
+  new Vue({
+    el: "#family",
+    data: {
+      llamaFact: "Llamas are members of the camelid family."
+    }
+  });
+
+  new Vue({
+    el: "#carry",
+    data: {
+      llamaFact: "Llamas can carry 500 percent of their body weight.",
+      fact: false
+    }
+  });
+
+  new Vue({
+    el: "#population",
+    data: {
+      llamaFact: "The current population of llamas and alpacas in South America is estimated to be more than 7 million."
+    }
+  });
+
+  new Vue({
+    el: "#diet",
+    data: {
+      llamaFact: "Llamas are vegetarians."
+    }
+  });
+
+  new Vue({
+    el: "#stomach",
+    data: {
+      llamaFact: "A llama's stomach has three compartments, called the rumen, omasum, and abomasum. "
+    }
+  });
+
+  new Vue({
+    el: "#poop",
+    data: {
+      llamaFact: "Llama poop has almost no odor, and makes for eco-friendly fertilizer."
+    }
+  });
+
+  new Vue({
+    el: "#lifespan",
+    data: {
+      llamaFact: "Llamas live to be about 20 years old."
+    }
+  });
+
+  new Vue({
+    el: "#relatives",
+    data: {
+      llamaFact: "Llamas have two wild cousins that have never been domesticated: the vicuña and the guanaco."
+    }
+  });
