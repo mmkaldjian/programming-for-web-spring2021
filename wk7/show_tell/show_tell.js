924172b3969354b6db2657df9a82a0386a55d472
mmkaldjian <74389945+mmkaldjian@users.noreply.github.com>
2021-03-07 15:26:58 -0600

    show and tell wk 7 final commit

--- wk7/show_tell/show_tell.js
+++ wk7/show_tell/show_tell.js
@@ -1,33 +1,33 @@
 const printArray = [{
-	image: "img/img/schwartz_gestureXIV.jpg"
+	image: "img/schwartz_gestureXIV.jpg",
     title: "Gesture XIV",
     artist: "Linda Schwartz",
     year: 2002,
-    edition: "Printer's Proof",
+    edition: "Trial Proof",
     medium: "Photolithography, hand painted & varnish"
 },{
-	image: "img/img/marks_butterflyweed.jpg"
+	image: "img/marks_butterflyweed.jpg",
     title: "Butterfly Weed",
     artist: "Mike Marks",
     year: 2021,
-    edition: "1/1",
+    edition: "1/3",
     medium: "woodcut"
 },{
-	image: "img/img/lin_lakestreet.jpg"
-    title: "Lake Street, Minneapolis, MN",
+	image: "img/lin_lakestreet.jpg",
+    title: "Lake Street, Minneapolis",
     artist: "Cori Lin",
     year: 2019,
     edition: "uneditioned",
-    medium: "Inkjet print of original watercolor illustration"
+    medium: "Inkjet print of watercolor illustration"
 },{
-	image: "img/img/nuzzo_lie.jpg"
+	image: "img/nuzzo_lie.jpg",
     title: "Lie",
     artist: "Jacqueline Nuzzo",
     year: 2019,
     edition: "1/5",
-    medium: "Digitized illustration on Hahnemühle Photo Rag Bright White"
+    medium: "Digitized illustration on Hahnemühle Photo Rag"
 },{
-	image: "img/img/sheely_redflower.jpg"
+	image: "img/sheely_redflower.jpg",
     title: "Red Flower",
     artist: "Landon Sheely",
     year: 2018,
