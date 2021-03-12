a65a0c1cbbd1d4c0d42c73b42fb0ac4e27c4cad2
mmkaldjian <74389945+mmkaldjian@users.noreply.github.com>
2021-03-12 00:04:11 -0600

    exercise 8.2 initial and final commit

---
+++ wk8/exercise8.2/script.js
@@ -0,0 +1,33 @@
+var vm = new Vue ({
+    el: "#calcApp",
+    data: {
+        firstNum: null,
+        secondNum: null,
+        operation: 'add',
+        total: null,
+    },
+    methods: {
+        calculate: (num1, num2) => {
+            switch (vm.operation) {
+                case 'add': 
+                    vm.total = num1 + num2;
+                    break;
+                case 'subtract':
+                    vm.total = num1 - num2;
+                    break;
+                case 'multiply':
+                    vm.total = num1 * num2;
+                    break;
+                case 'divide':
+                    vm.total = num1 / num2;
+                    break;
+
+                default: 
+                    window.alert('unexpected operator');
+                    break;
+            }
+
+            vm.firstNum = vm.secondNum = null // executed right to left; second is assigned to null, then first is assigned to second, which is null
+        }
+    }
+});
