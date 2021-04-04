b1dd2a0b10193b8e86c7c512e0512b9cc04ec588
ken-teaches-web <kkorth@mcad.edu>
2021-04-03 19:39:05 -0500

    needed drinks

--- wk10/assignment/script.js
+++ wk10/assignment/script.js
@@ -1,5 +1,5 @@
 Vue.component('cocktail-list', {
-    template: `<div class="cocktail"> 
+  template: `<div class="cocktail"> 
         <h3>{{cocktail.strDrink}}</h3>
         <div><img v-bind:src="cocktail.strDrinkThumb" alt=""></div>
         <p>{{cocktail.strIngredient1}}</p>
@@ -7,24 +7,24 @@ Vue.component('cocktail-list', {
         <p>{{cocktail.strIngredient3}}</p>
         <p>Instructions: {{cocktail.strInstructions}} and serve in a {{cocktail.strGlass}}.</p>
     </div>`,
-    props: ['cocktail'],
-    
+  props: ['cocktail'],
+
 });
 
 const vm = new Vue({
-    el: "#cocktailApp",
-    data: {
-        cocktails: []
-    },
+  el: "#cocktailApp",
+  data: {
+    cocktails: []
+  },
 
-    mounted () { /*similar to 'draw' of p5js*/
-        axios
-            .get('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=z') /*use get to fetch data*/
-            .then(response => { /*response after get it*/
-                console.log('response', response);
-                vm.cocktails = response.data;
-                console.log(vm.cocktails);
-                });
-    }
+  mounted() { /*similar to 'draw' of p5js*/
+    axios
+      .get('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=z') /*use get to fetch data*/
+      .then(response => { /*response after get it*/
+        console.log('response', response);
+        vm.cocktails = response.data.drinks; // it wasn't very obvious but you needed to get the drinks array from the response
+        console.log(vm.cocktails);
+      });
+  }
 })
 
